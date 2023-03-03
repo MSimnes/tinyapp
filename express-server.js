03b9ba23c79e7d151b0e02aed5c1500fd28dab26
Mike <mike.simnes@gmail.com>
2023-03-02 19:33:44 -0800

    add a touch of html to /hello

--- express-server.js
+++ express-server.js
@@ -6,15 +6,19 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
 
 app.get('/', (req, res) => {
   res.send('Hello');
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
-});
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get('/hello', (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
