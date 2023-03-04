00a4ee717151681ddf4274615a89377cf2c158ee
Mike <mike.simnes@gmail.com>
2023-03-03 20:48:42 -0800

    add GET and POST routes to server

--- express-server.js
+++ express-server.js
@@ -3,6 +3,18 @@ const app = express();
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+const generateRandomString = function() {
+  let result = "";
+  const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUV123456789";
+  const charLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charLength));
+  }
+  return result;
+};
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -27,6 +39,15 @@ app.get('/hello', (req, res) => {
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get('/urls/:id', (req, res) => {
