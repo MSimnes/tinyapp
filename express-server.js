454fea7d4528eac40bd933b6a60a92029047f9e2
Mike <mike.simnes@gmail.com>
2023-03-06 10:43:33 -0800

    feature branch: add login route with redirect to index

--- express-server.js
+++ express-server.js
@@ -6,8 +6,8 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
 const generateRandomString = function() {
-  let result = "";
-  const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUV123456789";
+  let result = '';
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUV123456789';
   const charLength = characters.length;
   for (let i = 0; i < 6; i++) {
     result += characters.charAt(Math.floor(Math.random() * charLength));
@@ -16,8 +16,8 @@ const generateRandomString = function() {
 };
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com'
 };
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
@@ -32,27 +32,27 @@ app.get('/urls.json', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 // display list of shortened urls
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  res.render('urls_index', templateVars);
 });
 // update database and redirect to the id's own page
-app.post("/urls", (req, res) => {
+app.post('/urls', (req, res) => {
   const newId = generateRandomString();
   urlDatabase[newId] = req.body.longURL;
   res.redirect(`urls/${newId}`);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
+  res.render('urls_show', templateVars);
 });
 
 // redirect to actual site of long URL
@@ -64,12 +64,18 @@ app.get('/u/:id', (req, res) => {
 // route for delete button and removal from database
 app.post('/urls/:id/delete', (req, res) => {
   delete urlDatabase[req.params.id];
-  res.redirect("/urls");
+  res.redirect('/urls');
 });
 // route to update longURL from form input and redirect to index
 app.post('/urls/:id/update', (req, res) => {
   const id = req.params.id;
   const newURL = req.body.longURL;
   urlDatabase[id] = newURL;
-  res.redirect("/urls");
+  res.redirect('/urls');
+});
+// route for login form
+app.post('/urls/login', (req, res) => {
+  const cookie = req.body.username;
+  console.log(cookie);
+  res.cookie('username', cookie).redirect('/urls');
 });
