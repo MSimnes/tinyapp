ac61dd84f031391dc5fe201c4976f43c39f9b0d0
Mike <mike.simnes@gmail.com>
2023-03-13 16:52:48 -0700

    move helper function to modules file

---
+++ helpers.js
@@ -0,0 +1,13 @@
+
+const getUserByEmail = function(email, database) {
+  for (let user in database) {
+    if (email === database[user].email) {
+      return database[user];
+    }
+  }
+  return null;
+};
+
+module.exports = {
+  getUserByEmail
+};
